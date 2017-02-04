4e9cb776a3b1e18af2cebd73dc14f5e9ffa8efe6
Syed Huda <syed.huda@gmail.com>
2017-02-04 12:34:04 +0000

    Send html file at /

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var app = express();
 
 var jsonData = {count: 12, message: 'hey'};
 
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html', function(error) {
+    if (error) {
+      res.status(500).send(error);
+    }
+  })
+});
+
 var port = 3000;
 app.listen(port, function() {
   console.log('Listening on http://localhost:3000');
