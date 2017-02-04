4e697fdcaeb469072fd2d76669a4bcd01369f1b0
Syed Huda <syed.huda@gmail.com>
2017-02-04 12:43:19 +0000

    Added get all lions and post a lion to /lions endpoint.

--- server/server.js
+++ server/server.js
@@ -22,6 +22,18 @@ var lions = [];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+app.get('/lions', function(req, res) {
+    res.json(lions);
+});
+
+app.post('/lions', function(req, res) {
+  var lion = req.body;
+  id++;
+  lion.id = id + '';
+
+  lions.push(lion);
+  res.json(lion);
+});
 
 app.listen(3000);
 console.log('on port 3000');
