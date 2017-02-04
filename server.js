35e5da28c32bf2c6d5805501402f836defc452e6
Syed Huda <syed.huda@gmail.com>
2017-02-04 12:29:04 +0000

    Added express to server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-// TODO: create a basic server with express
-// that will send back the index.html file on a GET request to '/'
-// it should then send back jsonData on a GET to /data
+var express = require('express');
+var app = express();
 
 var jsonData = {count: 12, message: 'hey'};
+
+var port = 3000;
+app.listen(port, function() {
+  console.log('Listening on http://localhost:3000');
+});
